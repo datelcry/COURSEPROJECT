6dba73b5c8f27ab932fde0f31244dffa750b969e
Egor <datelcry1995@gmail.com>
2016-09-25 14:27:49 +0300

    changes1

--- MVCsite/Controllers/AccountController.cs
+++ MVCsite/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace MVCsite.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Username, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Username};
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {

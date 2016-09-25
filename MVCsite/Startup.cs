6abe9c65bc1e33bbe1446a8c9f925131a4f076bd
Egor <datelcry1995@gmail.com>
2016-09-25 13:43:33 +0300

    start

---
+++ MVCsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCsite.Startup))]
+namespace MVCsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
